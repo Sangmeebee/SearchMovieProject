094bd79b93d0cc0883025817f0b344311867039e
Sangmeebee <apfhdznzl@gmail.com>
2022-12-06 19:33:19 +0900

    test: MovieRepository 즐겨찾는 영화 조회,저장,삭제 로직 추가에 따른 테스트 코드 수정 & 테스트

--- data/src/test/java/com/sangmeebee/searchmovieproject/data/repository/MovieRepositoryImplTest.kt
+++ data/src/test/java/com/sangmeebee/searchmovieproject/data/repository/MovieRepositoryImplTest.kt
@@ -1,21 +1,70 @@
 package com.sangmeebee.searchmovieproject.data.repository
 
+import com.sangmeebee.searchmovieproject.data.datasource.local.MovieLocalDatasource
 import com.sangmeebee.searchmovieproject.data.datasource.remote.MovieRemoteDatasource
+import com.sangmeebee.searchmovieproject.data.model.mapper.toData
+import com.sangmeebee.searchmovieproject.domain.model.BookmarkedMovie
 import com.sangmeebee.searchmovieproject.domain.repository.MovieRepository
+import io.mockk.coVerify
 import io.mockk.mockk
 import io.mockk.verify
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Test
 
 class MovieRepositoryImplTest {
 
+    private lateinit var movieRemoteDatasource: MovieRemoteDatasource
+    private lateinit var movieLocalDatasource: MovieLocalDatasource
+    private lateinit var movieRepositoryImpl: MovieRepository
+
+    @Before
+    fun setup() {
+        movieRemoteDatasource = mockk(relaxed = true)
+        movieLocalDatasource = mockk(relaxed = true)
+        movieRepositoryImpl = MovieRepositoryImpl(movieRemoteDatasource, movieLocalDatasource)
+    }
+
     @Test
     fun `데이터소스에 영화 정보 조회를 요청한다`() {
-        // given
-        val movieRemoteDatasource: MovieRemoteDatasource = mockk(relaxed = true)
-        val movieRepositoryImpl: MovieRepository = MovieRepositoryImpl(movieRemoteDatasource)
         // when
         movieRepositoryImpl.getMovies(query = "광해", pageSize = 20)
         // then
         verify { movieRemoteDatasource.getMovies(query = "광해", pageSize = 20) }
+    }
+
+    @Test
+    fun `데이터소스에 즐겨찾는 영화 조회 요청한다`() {
+        // when
+        movieRepositoryImpl.getBookmarkedMovies()
+        // then
+        verify { movieLocalDatasource.getBookmarkedMovies() }
+    }
+
+    @Test
+    fun `데이터소스에 즐겨찾는 영화 저장을 요청한다`() = runTest {
+        // when
+        movieRepositoryImpl.postBookmarkedMovie(fakeMovie)
+        // then
+        coVerify { movieLocalDatasource.postBookmarkedMovie(fakeMovie.toData()) }
+    }
+
+    @Test
+    fun `데이터소스에 즐겨찾는 영화 삭제를 요청한다`() = runTest {
+        // when
+        movieRepositoryImpl.deleteBookmarkedMovie(fakeMovie)
+        // then
+        coVerify { movieLocalDatasource.deleteBookmarkedMovie(fakeMovie.toData()) }
+    }
+
+    companion object {
+        private val fakeMovie = BookmarkedMovie(
+            title = "<b>광해</b>, 왕이 된 남자",
+            link = "https://movie.naver.com/movie/bi/mi/basic.nhn?code=83893",
+            imageUrl = "https://ssl.pstatic.net/imgmovie/mdi/mit110/0838/83893_P09_112146.jpg",
+            director = "추창민",
+            actor = "이병헌,류승룡,한효주",
+            userRating = "9.25"
+        )
     }
 }
