60899979ccef2b825fa282ad0603eb43b664d579
Sangmeebee <apfhdznzl@gmail.com>
2022-12-16 13:25:39 +0900

    feat: 검색어를 MutableSharedFlow로 변환

--- presentation/src/main/java/com/sangmeebee/searchmovieproject/presentation/ui/screen/searchmovie/SearchMovieViewModel.kt
+++ presentation/src/main/java/com/sangmeebee/searchmovieproject/presentation/ui/screen/searchmovie/SearchMovieViewModel.kt
@@ -16,13 +16,13 @@ import com.sangmeebee.searchmovieproject.presentation.model.mapper.toPresentatio
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
@@ -41,14 +41,11 @@ class SearchMovieViewModel @Inject constructor(
     private val _uiState = MutableStateFlow(SearchMovieUiState())
     val uiState = _uiState.asStateFlow()
 
-    private val query = MutableStateFlow<String?>(null)
+    private val query = MutableSharedFlow<String>()
 
     @OptIn(ExperimentalCoroutinesApi::class)
     private val searchMovies: Flow<PagingData<MovieModel>> =
         query.flatMapLatest { query ->
-            if (query == null) {
-                return@flatMapLatest emptyFlow()
-            }
             getMovieFlowUseCase(query).map { pagingData ->
                 pagingData.map { movieInfo -> movieInfo.toPresentation() }
             }
